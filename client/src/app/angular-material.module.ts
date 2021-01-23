4f1a8ad368b4270aa23327fc39af41fbb0013f85
apanemia <andrewgphillips74@gmail.com>
2021-01-23 03:25:44 -0500

    initial front and back setup

---
+++ client/src/app/angular-material.module.ts
@@ -0,0 +1,80 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import {MatSelectInfiniteScrollModule} from 'ng-mat-select-infinite-scroll';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  MatSnackBarModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatButtonToggleModule,
+  MatDividerModule,
+  MatProgressBarModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MccColorPickerModule } from 'material-community-components';
+import { MatBadgeModule } from '@angular/material/badge';
+
+import { MatSelectSearchModule } from './components/mat-select-search/mat-select-search.module';
+
+@NgModule({
+  imports: [
+    MccColorPickerModule.forRoot({
+      empty_color: 'transparent',
+      used_colors: ['#000000', '#FFF555']
+    })
+  ],
+  exports: [
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatStepperModule,
+    MatSlideToggleModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSelectInfiniteScrollModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatListModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatDividerModule,
+    MccColorPickerModule,
+    MatBadgeModule,
+    MatSelectSearchModule,
+    MatProgressBarModule
+  ]
+})
+export class AngularMaterialModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+
+// Components
+import { AppComponent } from './app.component';
+
+// Apollo
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { ApolloLink } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
@@ -12,11 +18,37 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    ApolloModule,
+    HttpLinkModule,
     AppRoutingModule,
-    GraphQLModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    const cleanTypeName = new ApolloLink((operation, forward) => {
+      if (operation.variables) {
+        const omitTypename = (key, value) => {
+          return key === '__typename' ? undefined : value;
+        };
+        // operation.variables = JSON.parse(JSON.stringify(operation.variables), omitTypename);
+      }
+      return forward(operation).map((data) => {
+        return data;
+      });
+    });
+
+    const httpLinkWithErrorHandling = ApolloLink.from([
+      cleanTypeName,
+      httpLink.create({ uri: 'http://127.0.0.1/graphql'})
+    ]);
+    apollo.create({
+      link: httpLinkWithErrorHandling,
+      cache: new InMemoryCache({
+        addTypename: false
+      })
+    });
+  }
+}
