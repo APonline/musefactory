f489e072ce67522ca52fd5e9908580cd4da3c0ca
apanemia <andrewgphillips74@gmail.com>
2021-01-23 15:48:28 -0500

    wrapping up init

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ const driver = neo4j.driver(
     process.env.NEO4J_URI || `bolt://127.0.0.1:7687`,
     neo4j.auth.basic(
         process.env.NEO4J_USER || 'neo4j', 
-        process.env.NEO4J_PASSWORD || 'neo4j')
+        process.env.NEO4J_PASSWORD || 'neo4j'),
+    { encrypted: "ENCRYPTION_OFF"}
 );
 
 const server = new ApolloServer({
