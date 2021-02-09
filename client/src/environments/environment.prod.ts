5840789c6627799491d7909585d7a76f5ce7e9fa
apanemia <andrewgphillips74@gmail.com>
2021-02-08 20:56:59 -0500

    adding CI

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: 'https://musefactorry.app:4000/graphql',
+  apiSubUrl: 'ws://musefactorry.app:4000/subscriptions'
 };
