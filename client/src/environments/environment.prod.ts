f6fb05c0b96313ff1bf684b799837318bae33bf0
apanemia <andrewgphillips74@gmail.com>
2021-02-09 01:42:12 -0500

    updating cors

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://musefactorry.app:4000/graphql',
-  apiSubUrl: 'ws://musefactorry.app:4000/subscriptions',
+  apiUrl: 'https://musefactory.app:4000/graphql',
+  apiSubUrl: 'wss://musefactory.app:4000/subscriptions',
   version: 'v1.0.0'
 };
