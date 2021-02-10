2cc109e172920ce7cb1d69b1c0bdc888b7946a2f
apanemia <andrewgphillips74@gmail.com>
2021-02-10 03:14:44 -0500

    updating api ip details

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:4000/graphql',
-  apiSubUrl: 'ws://127.0.0.1:4000/subscriptions',
+  apiUrl: 'https://musefactory.app:4000/graphql',
+  apiSubUrl: 'wss://musefactory.app:4000/subscriptions',
   version: 'v1.0.0'
 };
